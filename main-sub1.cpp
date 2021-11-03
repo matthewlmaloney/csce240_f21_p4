f721d17594bf4c217db7b6b2719912cfa4801e68
coleca24 <coleca@email.sc.edu>
2021-11-03 08:47:31 -0400

    Update main-sub1.cpp

--- main-sub1.cpp
+++ main-sub1.cpp
@@ -1,1 +1,33 @@
+// Copyright 2021 <Your name here>
+#include <iostream>
+#include <vector>
+#include <string>
 
+using std::vector;
+using std::cout;
+using std::string;
+
+void readData(vector<string> &data, string filename);
+vector<int> findMinHamming(const vector<string> & data, string test);
+void printMinDistance(const vector<string> &data, string word, const vector<int> min);
+
+int main(int argc, char **argv) {
+  string filename;
+  vector<string> data;
+  
+  return 0; 
+}
+
+void readData(vector<string> &data, string filename) {
+  return;
+}
+
+vector<int> findMinHamming(const vector<string> & data, string test) {
+  vector<int> ret; 
+  
+  return ret;
+}
+
+void printMinDistance(const vector<string> &data, string word, const vector<int> min) {
+  return;
+}
