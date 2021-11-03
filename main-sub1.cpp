00c4a3bed73400988ef3961e712e5950a48b87a5
Casey Cole <coleca@email.sc.edu>
2021-11-03 10:48:16 -0400

    changes to mains and added txt files

--- main-sub1.cpp
+++ main-sub1.cpp
@@ -8,17 +8,33 @@ using std::cout;
 using std::string;
 
 void readData(vector<string> &data, string filename);
+void printData(const vector<string> &data);
 vector<int> findMinHamming(const vector<string> & data, string test);
-void printMinDistance(const vector<string> &data, string word, const vector<int> min);
+void printMinDistance(const vector<string> &data, string word,
+                      const vector<int> min);
 
 int main(int argc, char **argv) {
   string filename;
   vector<string> data;
-  
-  return 0; 
+  if (argc != 2) {
+    cout << "Usage: ./sub1 data.txt\n";
+    exit(0);
+  }
+  filename = argv[1];
+  readData(data, filename);
+  for (auto line : data) {
+    vector<int> ret;
+    ret = findMinHamming(data, line);
+    printMinDistance(data, line, ret);
+  }
+  return 0;
 }
 
 void readData(vector<string> &data, string filename) {
+  return;
+}
+
+void printData(const vector<string> &data) {
   return;
 }
 
@@ -28,6 +44,7 @@ vector<int> findMinHamming(const vector<string> & data, string test) {
   return ret;
 }
 
-void printMinDistance(const vector<string> &data, string word, const vector<int> min) {
+void printMinDistance(const vector<string> &data, string word,
+                      const vector<int> min) {
   return;
 }
--- main-sub2.cpp
+++ main-sub2.cpp
@@ -1,1 +1,71 @@
+// Copyright 2021 <Your name here>
+#include <iostream>
+#include <vector>
+#include <string>
 
+using std::vector;
+using std::cout;
+using std::string;
+
+void readPuzzle(vector<vector<char>> &puzzle, string filename);
+void readWords(vector<string> &words, string filename);
+void printPuzzle(const vector<vector<char>> &puzzle);
+void printWords(const vector<string> &words);
+void printLocation(string word, const vector<int> &start,
+                   const vector<int> &end, bool found);
+bool findWord(const vector<vector<char>> & puzzle, string word,
+              vector<int> &start, vector<int> &end);
+
+int main(int argc, char **argv) {
+  string puzzleFileName;
+  string wordsFileName;
+  vector<vector<char>> puzzle;
+  vector<string> words;
+  if (argc != 3) {
+    cout << "Usage: ./sub2 puzzle.txt words.txt\n";
+    exit(0);
+  }
+  puzzleFileName = argv[1];
+  wordsFileName = argv[2];
+  readPuzzle(puzzle, puzzleFileName);
+  readWords(words, wordsFileName);
+  printPuzzle(puzzle);
+  printWords(words);
+
+  for (auto word : words) {
+    vector<int> start;
+    vector<int> end;
+    bool found;
+    found = findWord(puzzle, word, start, end);
+    printLocation(word, start, end, found);
+    end.clear();
+    start.clear();
+  }
+  return 0;
+}
+
+void readPuzzle(vector<vector<char>> &puzzle, string filename) {
+    return;
+}
+
+void readWords(vector<string> &words, string filename) {
+    return;
+}
+
+void printPuzzle(const vector<vector<char>> &puzzle) {
+    return;
+}
+
+void printWords(const vector<string> &words) {
+    return;
+}
+
+void printLocation(string word, const vector<int> &start,
+                   const vector<int> &end, bool found) {
+    return;
+}
+
+bool findWord(const vector<vector<char>> & puzzle, string word,
+              vector<int> &start, vector<int> &end) {
+    return false;
+}
